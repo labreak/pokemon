5a02c6b21f9fa16358cf05dacfcebd9c9d786e13
Nicolas Padilla <padillanico@gmail.com>
2023-06-24 00:16:46 -0300

    se agrego incremental regeneration

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -123,18 +123,28 @@ const PokemonByName :NextPage<Props> = ({pokemon}) =>{
        paths:pokemonNames.map(name=>({
          params:{name}
        })),
-       fallback:false
+       fallback:'blocking'
      }
    }
    
    export const getStaticProps: GetStaticProps = async ({params}) => {
       const {name} = params as {name:string};   
-   
+      const pokemon=await getPokemonInfo(name)
         
+      if (!pokemon){
+        return {
+          redirect:{
+            destination:'/',
+            permanent:false
+          }
+        }
+      }
+      
      return {
        props: {
-        pokemon:await getPokemonInfo(name)
-       }
+        pokemon
+       },
+       revalidate:86400,
      }
    }
 
--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,15 +2,23 @@ import { pokeApi } from "@/api";
 import { Pokemon } from "@/interfaces";
 
 export const getPokemonInfo= async (nameorId:string)=>{
-    
+
+    try {
+
+        const {data} = await pokeApi.get<Pokemon>(`/pokemon/${nameorId.toLowerCase()}`);
+
+        return {
+             id:data.id,
+             name:data.name,
+             sprites:data.sprites
+         }
+      
+        
+    } catch (error) {
+        return null;
+    }
      
-    const {data} = await pokeApi.get<Pokemon>(`/pokemon/${nameorId}`);
-     return {
-          id:data.id,
-          name:data.name,
-          sprites:data.sprites
-      }
-   
+
 
 
 }
